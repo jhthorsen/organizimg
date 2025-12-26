c4263e54fb3b9e1f8ddb0853bb40eb29064a5a25
Jan Henning Thorsen <jan.henning@thorsenlabs.com>
2025-12-26 09:29:51 +0900

    Converted javascript code into a class

--- src/main.ts
+++ src/main.ts
@@ -13,156 +13,181 @@ type File = {
   size: number
 }
 
-type View = {
-  imageObserver: IntersectionObserver
-  path: string | null
-  $images: HTMLElement[]
-  $visibleImage: HTMLElement | null
-}
-
 const $ = (p: Document | HTMLElement, sel: string): HTMLElement | null => p.querySelector(sel)
-const $main = $(document, 'main')!
 
-const view: View = {
-  imageObserver: new IntersectionObserver(onImageIsVisible, {root: $main, threshold: 0.1}),
-  path: null,
-  $images: [],
-  $visibleImage: null,
-}
+class Organizeimg {
+  $root: HTMLElement
+  $visibleImage: HTMLElement | null
+  imageObserver: IntersectionObserver
 
-window.addEventListener('keydown', onKeyDown)
-$(document, 'button[name=trash]')!.addEventListener('click', trashImages)
-$(document, 'button[name=open]')!.addEventListener('click', openDirectory)
-
-function displayImages(images: File[]) {
-  view.$images = []
-  ;[].map.call($main.querySelectorAll('div.image'), (el: HTMLElement) => el.remove())
-
-  if (images.length === 0) {
-    return displayText('No images found.')
+  constructor($root: HTMLElement) {
+    this.$root = $root
+    this.$visibleImage = null
+    this.imageObserver = new IntersectionObserver(this._onImageIntersectChange.bind(this), {
+      root: $root,
+      threshold: 0.1,
+    })
   }
 
-  images.forEach((image) => {
-    const $image = document.createElement('div')
-    $image.dataset.path = image.path
-    $image.dataset.src = convertFileSrc(image.path)
-    $image.className = 'image'
-    $image.innerText = image.path.split('/').pop() || ''
-    $main.appendChild($image)
-    view.$images.push($image)
-    view.imageObserver.observe($image)
-  })
+  displayImages(images: File[]) {
+    ;[].map.call(this.$root.querySelectorAll('div.image'), (el: HTMLElement) => el.remove())
 
-  $main.classList.remove('empty')
-  $main.classList.add('images')
-}
+    if (images.length === 0) {
+      return this.displayMessage('No images found in selected directory.')
+    }
 
-function displayText(text: string) {
-  $($main, 'p')!.textContent = text
-  $main.classList.remove('images')
-  $main.classList.add('empty')
-}
-
-function onImageIsVisible(entries: IntersectionObserverEntry[]) {
-  for (const entry of entries) {
-    if (!entry.isIntersecting) continue
-    view.$visibleImage = entry.target as HTMLElement
-
-    const idx = view.$images.indexOf(view.$visibleImage)
-    view.$images.forEach(($image, i) => {
-      let $img = $image.firstElementChild as HTMLImageElement
-
-      if (i >= idx - 2 && i <= idx + 3) {
-        if (!$img) {
-          $img = document.createElement('img')
-          $img.alt = $image.textContent!.trim()
-          $img.src = $image.dataset.src ?? ''
-          $image.innerText = ''
-          $image.appendChild($img)
-        }
-      } else if ($img) {
-        $image.innerText = $img.alt
-      }
-
-      if (i === idx) {
-        $($main, '.name')!.innerText = $img.alt
-      }
+    images.forEach((image) => {
+      const $image = document.createElement('div')
+      $image.className = 'image'
+      $image.innerText = image.path.split('/').pop() || ''
+      $image.dataset.path = image.path
+      $image.dataset.src = convertFileSrc(image.path)
+      this.$root.appendChild($image)
+      this.imageObserver.observe($image)
     })
 
-    break
-  }
-}
-
-function onKeyDown(evt: KeyboardEvent) {
-  if (evt.key === 'ArrowRight' || evt.key === 'l') {
-    const idx = view.$images.indexOf(view.$visibleImage as HTMLElement)
-    view.$images[idx + 1]?.scrollIntoView({behavior: 'smooth', block: 'nearest', inline: 'start'})
-  } else if (evt.key === 'ArrowLeft' || evt.key === 'h') {
-    const idx = view.$images.indexOf(view.$visibleImage as HTMLElement)
-    view.$images[idx - 1]?.scrollIntoView({behavior: 'smooth', block: 'nearest', inline: 'start'})
-  } else if ((evt.ctrlKey || evt.metaKey) && evt.key === 'o') {
-    openDirectory()
-  } else if (evt.key === 'o') {
-    openDirectory()
-  } else if (evt.key === 'Backspace' || evt.key === 'd') {
-    trashImages()
-  } else if (evt.key === 'ArrowDown' || evt.key === 'j') {
-    view.$visibleImage?.classList.add('discard')
-    toggleTrashButtonDisabled()
-  } else if (evt.key === 'ArrowUp' || evt.key === 'k') {
-    view.$visibleImage?.classList.remove('discard')
-    toggleTrashButtonDisabled()
-  } else {
-    return
+    this.$root.classList.remove('empty')
+    this.$root.classList.add('images')
   }
 
-  evt.preventDefault()
-}
-
-async function openDirectory() {
-  const path = await open({
-    directory: true,
-    multiple: false,
-    title: 'Select a directory',
-  })
-
-  view.path = path ?? ''
-  $(document, '#title .path')!.textContent = `${view.path}`
-  if (!path) return
-
-  try {
-    const images: File[] = await invoke('get_images', {path})
-    displayImages(images)
-  } catch (err) {
-    displayText(`${path}: ${err}`)
+  displayMessage(message: string) {
+    $(this.$root, 'p')!.textContent = message
+    this.$root.classList.remove('images')
+    this.$root.classList.add('empty')
   }
-}
 
-function toggleTrashButtonDisabled() {
-  const $btn = $(document, 'button[name=trash]')! as HTMLButtonElement
-  $btn.disabled = $main.querySelectorAll('.image.discard').length === 0
-}
+  findAllImages() {
+    return Array.from(this.$root.querySelectorAll<HTMLElement>('.image'))
+  }
 
-async function trashImages() {
-  const $discard = $main.querySelectorAll<HTMLInputElement>('.image.discard')
-  if ($discard.length === 0) return
-
-  const answer = await message(`Delete ${$discard.length} images?`, {
-    title: 'Confirm Deletion',
-    kind: 'error',
-    buttons: 'OkCancel',
-  })
-
-  if (answer.toLowerCase() !== 'ok') return
-
-  try {
-    for (const $image of $discard) {
-      await invoke('trash_image', {path: $image.dataset.path})
-      $image.remove()
+  async openDirectory(path: string | null) {
+    if (path === null) {
+      path = await open({
+        directory: true,
+        multiple: false,
+        title: 'Select a directory',
+      }) ?? ''
     }
-  } catch (err) {
-    displayText(`${err}`)
+
+    $(document, '#title .path')!.textContent = `${path ?? ''}`
+
+    if (path.length > 0) {
+      try {
+        this.displayImages(await invoke('get_images', {path}))
+      } catch (err) {
+        this.displayMessage(`${path}: ${err}`)
+      }
+    }
   }
 
-  toggleTrashButtonDisabled()
+  showImage(idx: number | string) {
+    switch (idx) {
+      case 'previous':
+        idx = this.findAllImages().indexOf(this.$visibleImage as HTMLElement) - 1
+        break
+      case 'next':
+        idx = this.findAllImages().indexOf(this.$visibleImage as HTMLElement) + 1
+        break
+      default:
+        idx = idx as number
+    }
+
+    const $images = this.findAllImages()
+    if (idx < 0) idx = $images.length - 1
+    if (idx >= $images.length) idx = 0
+    this.$visibleImage = $images[idx]
+    this.$visibleImage?.scrollIntoView({behavior: 'smooth', block: 'nearest', inline: 'start'})
+  }
+
+  async trashImages() {
+    const $discard = this.$root.querySelectorAll<HTMLInputElement>('.image.discard')
+    if ($discard.length === 0) return
+
+    const answer = await message(`Delete ${$discard.length} images?`, {
+      title: 'Confirm Deletion',
+      kind: 'error',
+      buttons: 'OkCancel',
+    })
+
+    if (answer.toLowerCase() === 'ok') {
+      try {
+        for (const $image of $discard) {
+          await invoke('trash_image', {path: $image.dataset.path})
+          $image.remove()
+        }
+      } catch (err) {
+        this.displayMessage(`${err}`)
+      }
+    }
+
+    this._toggleTrashButtonDisabled()
+  }
+
+  _onImageIntersectChange(entries: IntersectionObserverEntry[]) {
+    for (const entry of entries) {
+      if (!entry.isIntersecting) continue
+      this.$visibleImage = entry.target as HTMLElement
+
+      const $images = this.findAllImages()
+      const idx = $images.indexOf(this.$visibleImage)
+      $images.forEach(($image, i) => {
+        let $img = $image.firstElementChild as HTMLImageElement
+
+        if (i >= idx - 2 && i <= idx + 3) {
+          if (!$img) {
+            $img = document.createElement('img')
+            $img.alt = $image.textContent!.trim()
+            $img.src = $image.dataset.src ?? ''
+            $image.innerText = ''
+            $image.appendChild($img)
+          }
+        } else if ($img) {
+          $image.innerText = $img.alt
+        }
+
+        if (i === idx) {
+          $(this.$root, '.name')!.innerText = $img.alt
+        }
+      })
+
+      break
+    }
+  }
+
+  _onKeyDown(evt: KeyboardEvent) {
+    if (evt.key === 'ArrowRight' || evt.key === 'l') {
+      evt.preventDefault()
+      this.showImage('next')
+    } else if (evt.key === 'ArrowLeft' || evt.key === 'h') {
+      evt.preventDefault()
+      this.showImage('previous')
+    } else if ((evt.ctrlKey || evt.metaKey) && evt.key === 'o') {
+      evt.preventDefault()
+      this.openDirectory(null)
+    } else if (evt.key === 'o') {
+      evt.preventDefault()
+      this.openDirectory(null)
+    } else if (evt.key === 'Backspace' || evt.key === 'd') {
+      evt.preventDefault()
+      this.trashImages()
+    } else if (evt.key === 'ArrowDown' || evt.key === 'j') {
+      evt.preventDefault()
+      this.$visibleImage?.classList.add('discard')
+      this._toggleTrashButtonDisabled()
+    } else if (evt.key === 'ArrowUp' || evt.key === 'k') {
+      evt.preventDefault()
+      this.$visibleImage?.classList.remove('discard')
+      this._toggleTrashButtonDisabled()
+    }
+  }
+
+  _toggleTrashButtonDisabled() {
+    const $btn = $(document, 'button[name=trash]')! as HTMLButtonElement
+    $btn.disabled = this.$root.querySelectorAll('.image.discard').length === 0
+  }
 }
+
+const organizeimg = new Organizeimg($(document, 'main')!)
+window.addEventListener('keydown', (evt) => organizeimg._onKeyDown(evt))
+$(document, 'button[name=trash]')!.addEventListener('click', () => organizeimg.trashImages())
+$(document, 'button[name=open]')!.addEventListener('click', () => organizeimg.openDirectory(null))
