c4d4b56f672f801862b791454f72c2784585ddc7
Jan Henning Thorsen <jan.henning@thorsenlabs.com>
2025-12-26 10:19:20 +0900

    Removed dead code

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -60,8 +60,6 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_window_state::Builder::new().build())
         .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_fs::init())
-        .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![get_images, trash_image])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
